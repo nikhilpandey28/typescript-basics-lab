310281a544221a2748dc4093dcee2a5c5b4c17cc
Nikhil <nikhil_pandey@epam.com>
2026-07-30 11:06:02 +0530

    Complete Section 2: Setting Up TypeScript

---
+++ src/02-setting-up-typescript/hello.ts
@@ -0,0 +1,7 @@
+let language : string = "TypeScript";
+
+function greet(name : string) : string{
+    return `Hello ${name}! , Welcome to ${language}`;
+}
+
+console.log(greet("Nikhil"));
---
+++ src/02-setting-up-typescript/typeError.ts
@@ -0,0 +1,3 @@
+let age: number = 22;
+
+// age = "Twenty Two"; Error: Type 'string' is not assignable to type 'number'.
